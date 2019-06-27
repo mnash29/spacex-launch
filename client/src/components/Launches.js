93850b2b4f2061ee783681b3d612a8b406b9137d
mnash29 <mtntrades10@gmail.com>
2019-06-27 15:19:35 -0700

    Added functional component MissionKey

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -2,6 +2,7 @@ import React from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import LaunchItem from './LaunchItem';
+import MissionKey from './MissionKey';
 
 const LAUNCHES_QUERY = gql`
 	query LaunchesQuery {
@@ -17,7 +18,15 @@ const LAUNCHES_QUERY = gql`
 export default function Launches() {
 	return (
 		<React.Fragment>
-			<h1 className="display-4 my-3">Launches</h1>
+			<div className="row my-3">
+				<div className="col-md-6">
+					<h1 className="display-4 py-3">Launches</h1>
+				</div>
+				<div className="col-md-6">
+					<MissionKey />
+				</div>
+			</div>
+			
 			<Query query={LAUNCHES_QUERY}>
 				{({ loading, error, data }) => {
 						if(loading) return <h4>Loading...</h4>;
@@ -35,4 +44,3 @@ export default function Launches() {
 		</React.Fragment>
 	);
 }
-
