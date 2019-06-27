401beb9661450ccf716326999a798bafe61fb6c1
mnash29 <mtntrades10@gmail.com>
2019-06-27 12:19:43 -0700

    Created Launches functional component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import Launches from './components/Launches'
 import logo from './SpaceX_Logo_Black.png';
 import './App.css';
 
+const client = new ApolloClient({
+	uri: 'http://localhost:5000/graphql'
+});
+
 function App() {
   return (
-    <div className="App mt-4">
-      <img src={logo} alt="SpaceX" style={{width: 525, display:'block', margin:'auto'}}/>
-    </div>
+  	<ApolloProvider client={client}>
+	    <div className="container mt-4">
+	      <img src={logo} alt="SpaceX" style={{width: 525, display:'block', margin:'auto'}}/>
+	    </div>
+	    <Launches />
+    </ApolloProvider>
   );
 }
 
