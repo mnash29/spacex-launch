264e50b8b310a381445022bbd77c8dfa674804d5
mnash29 <mtntrades10@gmail.com>
2019-07-01 16:07:25 -0700

    Finished launch details page

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
-//import { Link } from 'react-router';
-//import classNames from 'classnames';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
 
 const LAUNCH_QUERY = gql`
 	query LaunchQuery($flight_number: Int!) {
@@ -32,9 +32,40 @@ export default class Launch extends Component {
 							if(loading) return <h4>Loading...</h4>;
 							if(error) console.log(error);
 
-							console.log(data);
+							const { 
+								mission_name, 
+								flight_number, 
+								launch_year, 
+								launch_success, 
+								rocket: { rocket_id, rocket_name, rocket_type }
+							} = data.launch;
 
-							return <h1>Test</h1>;
+							return <div>
+								<h1 className="display-5 my-3"><span className="text-dark">Mission:</span>{' '}
+									{mission_name}
+								</h1>
+								<h4 className="mb-3">Launch Details</h4>
+									<ul className="list-group">
+										<li className="list-group-item">Flight Number: {flight_number}</li>
+										<li className="list-group-item">Launch Year: {launch_year}</li>
+										<li className="list-group-item">Launch Success:{' '}
+											<span className={classNames({
+												'text-success': launch_success,
+												'text-danger': !launch_success
+												})}>
+												{launch_success ? 'Yes' : 'No'}
+											</span>
+										</li>
+									</ul>
+									<h4 className="my-3">Rocket Details</h4>
+									<ul className="list-group">
+										<li className="list-group-item">Rocket ID: {rocket_id}</li>
+										<li className="list-group-item">Rocket Name: {rocket_name}</li>
+										<li className="list-group-item">Rocket Type: {rocket_type}</li>
+									</ul>
+									<hr/>
+									<Link to="/" className="btn btn-outline-info">Back</Link>
+							</div>;
 						}}
 				</Query>
 			</React.Fragment>
